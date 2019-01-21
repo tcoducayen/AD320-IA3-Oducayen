712daf8742532d23957f17ec0cb6693bb077a8cb
Thomas <tcoducayen@gmail.com>
2019-01-21 15:56:52 -0800

    updating all the current files in folder

---
+++ AD320-IA2-Oducayen/formhandler.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = $email = $tel = $state = "";
+
+echo "Input:" ;
+echo "Name : ";
+echo $name;
+echo <br>;
+echo "Email : ";
+echo $email;
+echo <br>;
+echo "Phone Number : ";
+echo $tel;
+echo <br>;
+echo "State : ";
+echo $state;
+echo <br>;
+
+
+?>
